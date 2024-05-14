e1411d48c9272caa0e43cf8485c8f6d804a5291c
jason.lai <jasonlai1218@gmail.com>
2024-05-14 23:44:48 +0800

    feat: improve file handling with timeout parameter in FileSensor
    - Add a `timeout` parameter to the `FileSensor` constructor
    - Update the `poke` method to handle timeouts while checking for file existence
    
    Signed-off-by: jason.lai <jasonlai1218@gmail.com>

--- flytekit/sensor/file_sensor.py
+++ flytekit/sensor/file_sensor.py
@@ -1,14 +1,21 @@
-from flytekit import FlyteContextManager
+import asyncio
+
+from flytekit import FlyteContextManager, logger
 from flytekit.sensor.base_sensor import BaseSensor
 
 
 class FileSensor(BaseSensor):
-    def __init__(self, name: str, **kwargs):
+    def __init__(self, name: str, timeout: int = 600, **kwargs):
         super().__init__(name=name, **kwargs)
+        self.timeout = timeout
 
     async def poke(self, path: str) -> bool:
         file_access = FlyteContextManager.current_context().file_access
         fs = file_access.get_filesystem_for_path(path, asynchronous=True)
-        if file_access.is_remote(path):
-            return await fs._exists(path)
-        return fs.exists(path)
+        try:
+            if file_access.is_remote(path):
+                return await asyncio.wait_for(fs._exists(path), timeout=self.timeout)
+            return fs.exists(path)
+        except asyncio.TimeoutError:
+            logger.info(f"Timeout reached while checking for file at {path}")
+            return False
